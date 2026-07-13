1d5ae01561bd81beaa48a67bf8ed657716a94510
Zubicoa Garijo <rzubicoa@minsait.com>
2026-07-13 15:14:12 +0200

    wip

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './layout/footer/footer.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { SidenavComponent } from './layout/sidenav/sidenav.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  template: '<router-outlet />',
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, SidenavComponent],
+  templateUrl: './app.html',
 })
 export class App {}
